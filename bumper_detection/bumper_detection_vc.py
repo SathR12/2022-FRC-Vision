3ca1362520bb8612f9c2da1e1744ee9e40f96805
SathR12 <74515743+SathR12@users.noreply.github.com>
2022-03-14 11:06:33 -0400

    Update bumper_detection_vc.py
    Uses PyVision Library and Pytesseract OCR

--- bumper_detection/bumper_detection_vc.py
+++ bumper_detection/bumper_detection_vc.py
@@ -16,11 +16,10 @@ import detect_colors
 import edit
 import object_info
 
-#image
-frame = cv.imread(r"/Users/cassini/Desktop/blue_rect.jpeg")
-
 #tesseract path differs for each operating system
 text_extraction.setPath(r'/Usr/local/bin/tesseract')
+
+camera = cv.VideoCapture(0)
 
 #HSV Values for Blue Bumper 
 LOWER_1 = [100, 150, 0]
@@ -42,13 +41,12 @@ def detectBlueBumper(frame):
     mask_blue = cv.morphologyEx(mask_blue, cv.MORPH_CLOSE, (7,7))
     mask_blue = cv.morphologyEx(mask_blue, cv.MORPH_OPEN, (7,7))
     contours = contour_features.getContours(mask_blue)
-    digits = text_extraction.extractText(mask_blue)
+    digits = len(text_extraction.extractText(mask_blue))
     for contour in contours:
         x, y, w, h = cv.boundingRect(contour)
-        if cv.contourArea(contour) > 1000 and 4 >= w/h >= 1.9 and len(digits) >= 2:
+        if cv.contourArea(contour) > 1000 and 4 >= w/h >= 1.9 and digits >= 2:
             contour_features.drawBoundingRect(frame, contour)
             cv.putText(frame, "Blue Bumper", (x, y - 10), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-            print(digits)
             
 def detectRedBumper(frame):
     global mask_red
@@ -56,29 +54,30 @@ def detectRedBumper(frame):
     mask_red = cv.morphologyEx(mask_red, cv.MORPH_CLOSE, (7,7))
     mask_red = cv.morphologyEx(mask_red, cv.MORPH_OPEN, (7,7))
     contours = contour_features.getContours(mask_red)
-    digits = text_extraction.extractText(mask_red)
+    digits = len(text_extraction.extractText(mask_red))
     for contour in contours:
         x, y, w, h = cv.boundingRect(contour)
-        if cv.contourArea(contour) > 1000 and 4 >= w/h >= 1.9 and len(digits) >= 2:
+        if cv.contourArea(contour) > 1000 and 2.4 >= w/h >= 1.9 and digits >= 2:
             contour_features.drawBoundingRect(frame, contour)
             cv.putText(frame, "Red Bumper", (x, y - 10), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-            print(digits)
     
     
     
     
-
-
-detectBlueBumper(frame)
-detectRedBumper(frame)
+while True:
+    ret, frame = camera.read()
+    detectBlueBumper(frame)
+    detectRedBumper(frame)
    
+    
+    cv.imshow('frame', frame)
+    key = cv.waitKey(1)
+    if key == 27:
+        break
+    
 
-cv.imshow('frame', frame)
-cv.imshow('red mask', mask_red)
-key = cv.waitKey(0)
-
-
-
+cv.destroyAllWindows()
+camera.release()
 
 
 
