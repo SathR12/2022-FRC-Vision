2ffdb1289d419d345a78ddb1f6b7a4394ecee4b6
SathR12 <74515743+SathR12@users.noreply.github.com>
2022-04-01 14:58:45 -0400

    Create objects.py

---
+++ camera/objects.py
@@ -0,0 +1,10 @@
+#This file contains examples of camera objects and is not intended for anything important
+
+cam1 = Camera("Logitech c720", 0)
+cam2 = Camera("Gopro 5", 0)
+cam2.setDimensions(1000, 500)
+cam1.setDimensions(640, 480)
+cam1.setExposure(-7)
+cam1.setSource(0)
+cam1.initialize()
+cam1.displayFeed()
